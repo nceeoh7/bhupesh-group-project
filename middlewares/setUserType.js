9d51133404089c8b9fe8d380892c1ec6bb9d84db
nceeoh7 <51413645+nceeoh7@users.noreply.github.com>
2022-12-09 16:08:47 -0500

    User Type loggin

--- middlewares/setUserType.js
+++ middlewares/setUserType.js
@@ -1,5 +1,7 @@
 module.exports = (req, res, next) => {
   if (req.session.userType) {
+    console.log("User Type Middleware");
+    console.log(req.session);
     userType = req.session.userType;
   }
   next();
